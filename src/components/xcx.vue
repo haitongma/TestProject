f60ff81cd89de079ae0402ff61f977330154b31a
麻海通 <haitong.ma@corp.elong.com>
2018-02-24 18:20:55 +0800

    xcx start

---
+++ src/components/xcx.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      开发者工具编辑器快捷键
+      1.代码格式化:⇧ + ⌥ + F
+      2.项目内搜索:⇧ + ⌘ + F
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+        }
+    }
+</script>
+<style scoped>
+  body{
+    background-color:#ff0000;
+  }
+</style>
